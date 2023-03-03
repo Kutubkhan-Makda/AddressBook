fcc517ad77586087270f9899120ff7f7a8c62b4b
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-03-03 23:29:13 +0530

    day-5

--- Models/LOC_CityModel.cs
+++ Models/LOC_CityModel.cs
@@ -1,18 +1,21 @@
-﻿namespace AddressBook.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace AddressBook.Models
 {
     public class LOC_CityModel
     {
         public int? CityID { get; set; }
+        [Required(ErrorMessage = "City Name is Required")]
         public string CityName { get; set; }
         public string CityCode { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime ModificationDate { get; set; }
         public int StateID { get; set; }
+        [Required(ErrorMessage = "Select State")]
         public string StateName { get; set; }
         public int CountryID { get; set; }
+        [Required(ErrorMessage = "Select Country")]
         public string CountryName { get; set; }
-        public IFormFile File { get; set; }
-        public string PhotoPath { get; set; }
     }
     public class LOC_CityDropDownModel
     {
